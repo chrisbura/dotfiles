5bf2d207041a59eb40d36217975ad8e29386a012
chrisbura <christopher.bura@gmail.com>
2011-06-07 21:06:33 -0400

    Add .gitignore file and pathogen.vim Update symlink folder names

--- setup.bat
+++ setup.bat
@@ -1,2 +1,2 @@
-mklink "C:\Users\Chris\_vimrc" "D:\Programming\Environment\Vim\_vimrc"
-mklink /D "C:\Users\Chris\vimfiles" "D:\Programming\Environment\Vim\vimfiles"
+mklink "C:\Users\Chris\_vimrc" "D:\Programming\Environment\dotfiles\_vimrc"
+mklink /D "C:\Users\Chris\vimfiles" "D:\Programming\Environment\dotfiles\vimfiles"
